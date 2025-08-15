7500193b2e515558ea13532a5790de4b02d4afc2
“Shashank-21” <shashank@smartalgorhythm.com>
2025-08-15 20:55:09 +0530

    Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,10 +20,14 @@ import { validateSession } from "@/actions";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const { user } = await validateSession();
+  try {
+    const { user } = await validateSession();
 
-  if (user) {
-    redirect("/dashboard");
+    if (user) {
+      redirect("/dashboard");
+    }
+  } catch (error) {
+    console.log(error);
   }
 
   return (
